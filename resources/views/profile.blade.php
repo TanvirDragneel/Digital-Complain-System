5188ad1f48471e8036059e1964597a93200abcc9
saiful29 <shaifulbd123@gmail.com>
2019-07-08 00:39:34 +0600

    update 7-7-19 night

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,24 +1,30 @@
-@extends('layouts.master')
+@extends('layouts.app')
 @section('content')
    {{-- {{$profile_data}}--}}
-   @foreach($profile_data as $data)
+   @if(\Session::has('message'))
+       <div class="alert alert-success w-75">
+           <p>{{$id = \Session::get('message')}}</p>
+       </div>
+   @endif
+   @foreach($profile_data as $data)  <center>
     <div class="content">
         <div class="container-fluid">
          <div class="row">
+
             <div class="col-lg-8 col-md-7">
                 <div class="card">
                     <div class="header">
                         <h4 class="title">Edit Profile</h4>
                     </div>
                     <div class="content">
-                        <form action="{{route('profile.update',[$data['id']])}}">
+                        <form action="{{route('profile.myupdate',[$data['id']])}}" method="post">
                             @csrf
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label>Name</label>
+                                        <label for="name">Name</label>
+           <input type="text" name="name" id="name"  class="form-control border-input" value="{{$data['name']}}">
 
-                                        <input name="name" id="name" type="text" class="form-control border-input" value="{{$data['name']}}">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
@@ -34,8 +40,8 @@
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="form-group">
-                                        <label>Address</label>
-                                        <input type="text" class="form-control border-input" placeholder="Home Address" value="">
+                                        <label for="address">Address</label>
+                                        <input type="text" id="address" name="address" class="form-control border-input" placeholder="Home Address" value="">
                                     </div>
                                 </div>
                             </div>
@@ -43,8 +49,8 @@
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                        <label>Registration</label>
-                                        <input type="text" disabled class="form-control border-input" value="{{$data['regid']}}">
+                                        <label for="reg">Registration</label>
+                                        <input type="text" id="reg" name="reg" disabled class="form-control border-input" value="{{$data['regid']}}">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
@@ -55,8 +61,8 @@
                                 </div>
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                        <label>Postal Code</label>
-                                        <input type="number" disabled class="form-control border-input" value="{{$data['permission']}}">
+                                        <label for="number">Postal Code</label>
+                                        <input type="number" id="number" name="number" disabled class="form-control border-input" value="{{$data['permission']}}">
                                     </div>
                                 </div>
                             </div>
@@ -64,8 +70,8 @@
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="form-group">
-                                        <label>About Me</label>
-                                        <textarea rows="5" class="form-control border-input" placeholder="Here can be your description" value="Mike">Oh so, your weak rhyme
+                                        <label for="about">About Me</label>
+                                        <textarea name="about" id="about" rows="5" class="form-control border-input" placeholder="Here can be your description" value="Mike">Oh so, your weak rhyme
 You doubt I'll bother, reading into it
 I'll probably won't, left to my own devices
 But that's the difference in our opinions.</textarea>
@@ -83,7 +89,8 @@ But that's the difference in our opinions.</textarea>
 
 
         </div>
+
     </div>
-    </div>
+    </div></center>
     @endforeach
 @endsection
